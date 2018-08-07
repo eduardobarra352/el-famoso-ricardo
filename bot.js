a792048d37abb108649a8b20a3d2cb7929e3b74c
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-06 22:37:26 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,8 +18,8 @@ bot.on("message", message => {
     if (cmd === `${prefix}famoso`){
         message.channel.send("ricardo");
     }
-    if (cmd === `${prefix}say` + `messageArray`){
-        message.channel.send(`messageArray`);
+    if (cmd === `${prefix}say` + " "){
+        message.channel.send(" ");
     }
     else if (cmd === `${prefix}tm`){
         message.channel.send("https://media.discordapp.net/attachments/394205840804151308/398998022920470530/sketch-1515197879765.png");
