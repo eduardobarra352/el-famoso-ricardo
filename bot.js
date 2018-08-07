c42788570953b525336ccff2806321a72872a9f0
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-06 22:22:15 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,10 +21,10 @@ bot.on("message", message => {
     }
     else if (cmd === `${prefix}say` + messageReply){
         message.channel.send(messageReply);
-    }
-    else if (cmd === `${prefix}say`) {
+        if (cmd === `${prefix}say`) {
     	message.reply("note quedes blanco, **di algo**");
-  	}
+  	    }
+    }
     else if (cmd === `${prefix}tm`){
         message.channel.send("https://media.discordapp.net/attachments/394205840804151308/398998022920470530/sketch-1515197879765.png");
     }
