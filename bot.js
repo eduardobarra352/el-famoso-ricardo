3b36dcd106ea7b6f24b872d6d1fe4b4e467d8bcd
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-07 19:14:58 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,7 +42,7 @@ bot.on("message", message => {
 
 module.exports.run = async (bot, message, args) => {
 
-      if(>message.member.hasPermission("ADMINISTRATOR")) return;
+      if(!message.member.hasPermission("ADMINISTRATOR")) return;
       const sayMessage = args.join(" ");
       message.delete().catch();
       message.channel.send(sayMessage);
