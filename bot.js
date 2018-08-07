b4e925c68cd8b86c79d7078e011b3bcc3d576774
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-06 23:08:51 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", () => {
     console.log(`${bot.user.username} is online! `);
-    bot.change_presence(game = new Discord.Game(name ='tm'));
+    bot.game = new Discord.Game(name ='tm'));
 });
 
 bot.on("message", message => {
