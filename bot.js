04b3eaa467e3129014637047e2e00eb8517cea50
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-07 18:48:03 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,18 +17,24 @@ bot.on("message", message => {
     let prefix = '>';
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
-    let cmd2 = messageArray[1];
+    let cmd2 = messageArray[2];
     let args = messageArray.slice(1);
     
+    if (cmd === `${prefix}`){
+        message.channel.send("Si");
+    }
+    if (cmd === `<`){
+        message.channel.send("No");
+    }
     //comandos de prefix
     if (cmd === `${prefix}famoso`){
         message.channel.send("ricardo");
     }
     if (cmd2 === `${prefix}say`){
         message.reply('sigue elpendejoxd');
-        if (cmd === message){
-            message.channel.send('message');
-        }
+    }
+    else if (cmd === `${prefix}say`, args){
+        message.channel.send('message');
     }
     if (cmd === `${prefix}tm`){
         message.channel.send("https://media.discordapp.net/attachments/394205840804151308/398998022920470530/sketch-1515197879765.png");
