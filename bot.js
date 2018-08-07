d66abdf1c2547987c7c3b6a0f138e95eb80d82f8
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-06 20:01:40 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", () => {
     console.log(`${bot.user.username} is online! `);
-    bot.user.setActivity(`el famoso ricardo`);
 });
 
 bot.on("message", message => {
