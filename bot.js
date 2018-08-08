e1cd73e9fbe621ee88e7e00afaef748804406cf9
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-07 20:00:10 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,7 +34,6 @@ bot.on("message", message => {
            if(!message.user.hasPermission("ADMINISTRATOR")) return;
            message.delete().catch();
            message.channel.send(sayMessage);
-        }
     }
     if (cmd === `${prefix}tm`){
         message.channel.send("https://media.discordapp.net/attachments/394205840804151308/398998022920470530/sketch-1515197879765.png");
