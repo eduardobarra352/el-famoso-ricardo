ea2b9495842005d8504326bccb1b377069d31604
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-07 21:13:43 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,6 +40,10 @@ bot.on("message", message => {
     if (cmd === `${prefix}tm`){
         message.channel.send("https://media.discordapp.net/attachments/394205840804151308/398998022920470530/sketch-1515197879765.png");
     }
+    if (cmd === `${prefix}playing`){
+        let play = args.join(' ');
+        bot.user.setPresence({game: {name: play , type: 3}});
+    }
     if (cmd === `${prefix}paz`){
         message.channel.send("amemonos");
     }
