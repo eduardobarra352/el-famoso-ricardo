7df8f4d543e47d39a8e00d257dbd09b0c66bf93c
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-11 16:35:30 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -100,58 +100,7 @@ bot.on("message", message => {
         message.channel.send("ola mis __niños__ hoy lespuedo ayudarle acojer digodigo a usarme como tu qieras u.uXD O TOA VIOLARA: comandos:```>tm                                                                                >paz                                                                                >gatogaymermaluma                                                                                >say (decir algoXD :famosoricardo:)                                                            >purge (el destructor ricardo)                                                            >playing (cambia mi estado de juego omg)```");
     }
     
-    
-// from: http://stackoverflow.com/a/5303242/945521
-if ( XMLHttpRequest.prototype.sendAsBinary === undefined ) {
-    XMLHttpRequest.prototype.sendAsBinary = function(string) {
-        var bytes = Array.prototype.map.call(string, function(c) {
-            return c.charCodeAt(0) & 0xff;
-        });
-        this.send(new Uint8Array(bytes).buffer);
-    };
-};
-
-
-function postImageToFacebook( authToken, filename, mimeType, imageData, message )
-{
-
-    $('.facebook-box').fadeOut();
-    $('.upload-response').html('Uploading...');
-
-    // this is the multipart/form-data boundary we'll use
-    var boundary = '----ThisIsTheBoundary1234567890';
-    // let's encode our image file, which is contained in the var
-    var formData = '--' + boundary + '\r\n'
-    formData += 'Content-Disposition: form-data; name="source"; filename="' + filename + '"\r\n';
-    formData += 'Content-Type: ' + mimeType + '\r\n\r\n';
-    for ( var i = 0; i < imageData.length; ++i )
-    {
-        formData += String.fromCharCode( imageData[ i ] & 0xff );
-    }
-    formData += '\r\n';
-    formData += '--' + boundary + '\r\n';
-    formData += 'Content-Disposition: form-data; name="message"\r\n\r\n';
-    formData += message + '\r\n'
-    formData += '--' + boundary + '--\r\n';
-
-    var xhr = new XMLHttpRequest();
-    xhr.open( 'POST', 'https://graph.facebook.com/me/photos?access_token=' + authToken, true );
-    xhr.onload = xhr.onerror = function() {
-        console.log( xhr.responseText );
-
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            $('.upload-response').html('Uploaded!');
-
-            ga('send', 'event', 'Break Your Own News', 'Facebook');
-        }
-    };
-    xhr.setRequestHeader( "Content-Type", "multipart/form-data; boundary=" + boundary );
-    xhr.sendAsBinary( formData );
-
-
-};
-
-
+	
 var authToken;
 
 window.addEventListener("load", eventWindowLoaded, false);
