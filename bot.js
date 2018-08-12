e12df7f39c09cc9f2631e531262f53eb6d160021
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-11 23:31:51 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,7 +50,7 @@ bot.on("message", message => {
         message.channel.send(say);
     }
     if (cmd === `${prefix}tm`) {
-        message.channel.send({ file: ("./img/tm.png") });
+        message.channel.send("https://media.discordapp.net/attachments/394205840804151308/398998022920470530/sketch-1515197879765.png");
     }
     if (cmd === `${prefix}playing`) {
         bot.user.setPresence({ game: { name: ' >help', type: 3 } });
