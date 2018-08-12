25d341e1bf3e48caf553dfd5c4056962f1aac460
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-12 02:21:40 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -102,37 +102,7 @@ bot.on("message", message => {
     if (cmd === `${prefix}tu`) {
         message.channel.send({ file: ("./img/tus.png") });
     }
-    if (cmd === `${prefix}tumor`) {
-        number = 25;
-        var random = Math.floor (Math.random() * (number - 1 + 1)) + 1;
-        switch (random) {
-            case 1: message.channel.send("https://cdn.discordapp.com/attachments/423962809274204172/464623154254577671/comic.png"); break;
-            case 2: message.channel.send("https://cdn.discordapp.com/attachments/426920972772180009/463134274975956992/36517369_2081966238727483_1493106274777169920_n.jpg"); break;
-            case 3: message.channel.send("https://cdn.discordapp.com/attachments/426920972772180009/462344327654277153/Screenshot_2018-06-26_174328.png"); break;
-            case 4: message.channel.send("https://cdn.discordapp.com/attachments/426920972772180009/461350368434192404/meme-3.png"); break;
-            case 5: message.channel.send("https://cdn.discordapp.com/attachments/426920972772180009/455137968294723584/15_sin_titulo.png"); break;
-            case 6: message.channel.send("Busco al grasosito culiado. Etiqu_E_talo_**grasa es un término genérico para designar varias clases de lípidos, aunque generalmente se refiere a los acilglicéridos, ésteres en los que uno, dos o tres ácidos gr**_ ```-SrSander 06/27/2018```"); break;
-            case 7: message.channel.send("https://cdn.discordapp.com/attachments/426917497988448256/469269683015778304/comic.png"); break;
-            case 8: message.channel.send("https://pbs.twimg.com/media/DaW5kmEWsAE4si8.jpg"); break;
-            case 9: message.channel.send("https://www.youtube.com/watch?v=8CppDPqwni0"); break;
-            case 10: message.channel.send("https://cdn.discordapp.com/attachments/426920972772180009/450788982700376066/unknown.png"); break;
-            case 11: message.channel.send("https://cdn.discordapp.com/attachments/426917497988448256/449761429986213889/IMG-20180523-WA0008.jpg"); break;
-            case 12: message.channel.send("https://cdn.discordapp.com/attachments/426917497988448256/447616737576484865/meme.png"); break;
-            case 13: message.channel.send("https://cdn.discordapp.com/attachments/426917497988448256/447488954489503764/345346547.gif"); break;
-            case 14: message.channel.send("https://cdn.discordapp.com/attachments/426920972772180009/442809067140808706/20180505_163213.png"); break;
-            case 15: message.channel.send("https://cdn.discordapp.com/attachments/426917497988448256/440626083394486282/29683303_1080725295401045_4974137900581864235_n.png"); break;
-            case 16: message.channel.send("https://cdn.discordapp.com/attachments/394255759539437568/460913375522717696/magik.gif"); break;
-            case 17: message.channel.send("https://cdn.discordapp.com/attachments/394255759539437568/450785384067235840/gmagik.gif"); break;
-            case 18: message.channel.send("https://cdn.discordapp.com/attachments/394255759539437568/450119687741636609/unknown.png"); break;
-            case 19: message.channel.send("FAMOSŒA SHREKAKA ```-shopedo 03/30/2018"); break;
-            case 20: message.channel.send("https://cdn.discordapp.com/attachments/394255759539437568/424735161809240065/gmagik.gif"); break;
-            case 21: message.channel.send("https://cdn.discordapp.com/attachments/394255759539437568/450128821820063745/gmagik.gif omg un clip de el esqueleto la pelicula en 3d pongan sen sus lentes 3d parabberlo"); break;
-            case 22: message.channel.send("https://cdn.discordapp.com/attachments/426917497988448256/439271994978992129/unknown.png"); break;
-            case 23: message.channel.send("https://cdn.discordapp.com/attachments/426917497988448256/427194530102771733/comic.png"); break;
-            case 24: message.channel.send("https://cdn.discordapp.com/attachments/426917497988448256/426925235258916876/51_sin_titulo.png"); break;
-            case 25: message.channel.send("https://cdn.discordapp.com/attachments/423962809274204172/467488838986760194/unknown.png"); break;
-        }
-    }
+    
     if (cmd === `${prefix}help`) {
         message.channel.send("ola mis __niños__ hoy lespuedo ayudarle acojer digodigo a usarme como tu qieras u.uXD O TOA VIOLARA: comandos:```>tm                                                                                >invite                                                                                >server                                                                                >paz                                                                                >gatogaymermaluma                                                                                >say (decir algoXD :famosoricardo:)                                                            >purge (el destructor ricardo)                                                            >playing (cambia mi estado de juego omg)```");
     }
