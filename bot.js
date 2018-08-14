5d22ccbe3800328424d20ee94469a05b9d0e958f
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-14 20:28:39 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -107,9 +107,8 @@ bot.on("message", message => {
         message.reply("complemente el Tu para q dea un mensaje de la siguiente forma:```>tu [textXD]```");
         let image = { file: ("./img/tus.png") };
         let tus = args.join(' ');
-        let Image = image;
         if (cmd === `${prefix}tu` && tus) {
-              var converter = new Image ()
+              var converter
                 {
                         let Width = image.width('507');
                         let Height = image.height('431');
