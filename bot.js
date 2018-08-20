ea2e230035bc7d2da5969cfcaa3db31d6b9474dc
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-19 21:47:21 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ bot.on("ready", () => {
     bot.user.setPresence({ game: { name: play, type: 1 } });
 });
 
-bot.on("message", async message => {
+bot.on("message", message => {
 
     if (message.author.bot) return undefined;
     if (message.channel.type === "dm") return;
@@ -89,24 +89,6 @@ bot.on("message", async message => {
     }
     if (cmd === `${prefix}sans`) {
         message.channel.send("gaming");
-    }
-    if (cmd === `${prefix}play`) {
-        if (!message.member.voiceChannel) return message.channel.send(":x: No estas en un canal de voz, porfa lentra™");
-        if (message.guild.me.voiceChannel) return message.channel.send("uy pero ya estoi en elcanal de voz jej");
-        if (!args[0]) return message.reply("```>play [url]```");
-        let validate = await ytdl.validateURL(args[0]);
-        if (!validate) return message.reply(":no_entry: El Url es incorrecto o no es existente u.u");
-        let info = await ytdl.getInfo(args[0]);
-        let connection = await message.member.voiceChannel.join();
-        let dispatcher = await connection.play(ytdl(args[0], { filter: "audioonly" }));
-        message.channel.send(`:musical_note: Ahorita escuchando: ${info.title}`);
-    }
-    if (cmd === `${prefix}leave`) {
-        if (!message.member.voiceChannel) return message.channel.send(":x: No estas en un canal de voz, porfa lentra™");
-        if (!message.guild.me.voiceChannel) return message.reply(":no_entry: No puedo conectarme al tal canal de voz sorri");
-        if (message.guild.me.voiceChannelID !== message.member.voiceChannelID) return message.channel.send(":x: no estas en el canal de voz correcto,,,");
-        message.guild.me.voiceChannel.leave();
-        message.channel.send(":runner: Saliendo del canal de voz,,,").then(msg => msg.delete(2000));
     }
 
     if (cmd === `famoso`) {
