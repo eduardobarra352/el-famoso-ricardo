5eaad63aa6adfa61a3e9df265b21b7d4eb9f1d10
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-20 18:36:52 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,7 @@ bot.on("message", message => {
     //variantes
     const prefix = '>';
     const ownerID = process.env.ownerID;
+    const code = process.env.code;
     let msg = message.content.toLowerCase();
     let args = message.content.slice(prefix.lenght).trim().split(' ');
     let cmd = args.shift().toLowerCase();
@@ -206,218 +207,39 @@ bot.on("message", message => {
       }
     }
 
-//  if (cmd === `${prefix}breakingnews`) {
-//      message.reply("confusion, la forma en la q sehace es la sig:```>breakingnews [headline] [ticker] [imageurl]```");
-//      let imageurl = args.join(' ');
-//      function eventWindowLoaded() {
-//          canvasApp();
-//      }
-//
-//      function drawImageProp(context, img, x, y, w, h, offsetX, offsetY) {
-//
-//          if (arguments.length === 2) {
-//              x = y = 0;
-//              w = 1280;
-//              h = 720;
-//          }
-//
-//          /// default offset is center
-//          offsetX = offsetX ? offsetX : 0.5;
-//          offsetY = offsetY ? offsetY : 0.5;
-//
-//          /// keep bounds [0.0, 1.0]
-//          if (offsetX < 0) offsetX = 0;
-//          if (offsetY < 0) offsetY = 0;
-//          if (offsetX > 1) offsetX = 1;
-//          if (offsetY > 1) offsetY = 1;
-//
-//          var iw = img.width,
-//              ih = img.height,
-//              r = Math.min(w / iw, h / ih),
-//              nw = iw * r,   /// new prop. width
-//              nh = ih * r,   /// new prop. height
-//              cx, cy, cw, ch, ar = 1;
-//
-//          /// decide which gap to fill
-//          if (nw < w) ar = w / nw;
-//          if (nh < h) ar = h / nh;
-//          nw *= ar;
-//          nh *= ar;
-//
-//          /// calc source rectangle
-//          cw = iw / (nw / w);
-//          ch = ih / (nh / h);
-//
-//          cx = (iw - cw) * offsetX;
-//          cy = (ih - ch) * offsetY;
-//
-//          /// make sure source rectangle is valid
-//          if (cx < 0) cx = 0;
-//          if (cy < 0) cy = 0;
-//          if (cw > iw) cw = iw;
-//          if (ch > ih) ch = ih;
-//
-//          /// fill image in dest. rectangle
-//          context.drawImage(img, cx, cy, cw, ch, x, y, w, h);
-//      }
-//
-//      function canvasApp() {
-//
-//          var headline = "Something went viral online";
-//          var ticker = "\"Is this really news?\" asks commenter  |  5 million retweets in 1 hour already";
-//          var img = new Image();
-//
-//          var theCanvas = document.getElementById("canvasOne");
-//          var context = theCanvas.getContext("2d");
-//
-//          var formElement = document.getElementById("textBox");
-//          formElement.addEventListener("keyup", textBoxChanged, false);
-//
-//          var formElement2 = document.getElementById("tickerBox");
-//          formElement2.addEventListener("keyup", textBox2Changed, false);
-//
-//          var imageLoader = document.getElementById('imageLoader');
-//          imageLoader.addEventListener('change', handleImage, false);
-//
-//
-//          var imageObj = new Image();
-//          imageObj.src = 'overlay.png';
-//
-//
-//          drawScreen();
-//
-//          function drawScreen() {
-//
-//              //Background
-//              context.fillStyle = "#222222";
-//              context.fillRect(0, 0, theCanvas.width, theCanvas.height);
-//
-//
-//              //Image
-//              if (img.src) {
-//                  drawImageProp(context, img);
-//              }
-//
-//              //Live
-//              context.fillStyle = "rgba(194, 21, 15, 1.000)";
-//              context.fillRect(80, 40, 104, 60);
-//
-//              context.font = "700 36px Signika";
-//              context.fillStyle = "#FFFFFF";
-//              context.fillText('LIVE', 96, 84);
-//
-//              //Box
-//              context.fillStyle = "rgba(255,255,255,0.85)";
-//              context.fillRect(80, 510, 1200, 110);
-//
-//              //Clock
-//
-//              context.fillStyle = "#000";
-//              context.fillRect(80, 620, 100, 60);
-//
-//              today = new Date();
-//              var m = today.getMinutes();
-//              var h = today.getHours();
-//
-//              if (m < 10) {
-//                  m = "0" + m
-//              };
-//
-//              context.font = "700 28px Signika";
-//              context.fillStyle = "#FFFFFF";
-//              context.fillText((h + ":" + m), 96, 660);
-//
-//              //Breaking News Strap
-//              // Create gradient
-//              redgrd = context.createLinearGradient(0, 430, 0, 510);
-//
-//              // Add colors
-//              redgrd.addColorStop(0.000, 'rgba(109, 36, 39, 1.000)');
-//              redgrd.addColorStop(0.015, 'rgba(224, 54, 44, 1.000)');
-//              redgrd.addColorStop(0.455, 'rgba(194, 21, 15, 1.000)');
-//              redgrd.addColorStop(0.488, 'rgba(165, 10, 1, 1.000)');
-//              redgrd.addColorStop(1.000, 'rgba(109, 36, 39, 1.000)');
-//
-//              context.fillStyle = redgrd;
-//              context.fillRect(80, 430, 420, 80);
-//
-//              context.font = "700 48px Signika";
-//              context.fillStyle = "#FFFFFF";
-//              context.fillText('BREAKING NEWS', 100, 488);
-//
-//              //Text
-//              context.font = "700 72px Signika";
-//              context.fillStyle = "#000000";
-//              context.fillText(message.toUpperCase(), 100, 590);
-//
-//              //Ticker
-//              context.fillStyle = "#feeb1a";
-//              context.fillRect(180, 620, 1100, 60);
-//
-//              context.font = "700 28px Signika";
-//              context.fillStyle = "#000";
-//              context.fillText(ticker.toUpperCase(), 200, 660);
-//
-//              //Logo
-//              context.shadowColor = "rgba(0,0,0,0.7)";
-//              context.shadowOffsetX = 0;
-//              context.shadowOffsetY = 0;
-//              context.shadowBlur = 6;
-//              context.globalAlpha = 0.6;
-//              //context.drawImage(imageObj, 560, 20);
-//              context.font = "400 36px Signika";
-//              context.fillStyle = "#fff";
-//              context.fillText('breakyourownnews.com', 860, 80);
-//              context.globalAlpha = 1;
-//              context.shadowBlur = 0;
-//          }
-//          function handleImage(e) {
-//              var reader = new FileReader();
-//              reader.onload = function (event) {
-//                  img.onload = function () {
-//                      drawScreen();
-//                  }
-//                  img.src = event.target.result;
-//              }
-//              reader.readAsDataURL(e.target.files[0]);
-//          }
-//          if (cmd === `${prefix}breakingnews` && headline === (' ') && ticker === (' ') && imageurl === 'https://') {
-//              eventWindowLoaded();
-//              var converter = new HtmlToImageConverter
-//              {
-//                  Width = 1280,
-//                      Height = 720
-//              };
-//              var pngBytes = converter.GenerateImage(headline + ticker + imageurl, drawScreen);
-//              message.channel.SendFileAsync(new MemoryStream(pngBytes), "breakingnews.png");
-//          }
-//      }
-//  }
+    if (cmd === `${prefix}breakingnews`) {
+        try {
+            let commandFile = require(`./bp/breaking.js`);
+            commandFile.run(bot, message, args, pdfcrowd, clientee);
+        } catch(e) {
+            console.log(e.stack);
+        }
+    }
 
     if (cmd === `${prefix}help`) {
         message.channel.send('ola mis __niños__ hoy lespuedo ayudarle acojer digodigo a usarme como tu qieras u.uXD O TOA VIOLARA \ncomandos:```>tm \n>invite \n>server \n>paz \n \n-"Tumoristico": \n>famosisimo \n>detectorql \n>tu \n>tumor (100 variaciones distintas omg) \n>gatogaymermaluma \n \n-Funciones bknes: \n>say (decir algoXD) \n>purge (el destructor ricardo) \n>playing (cambia mi estado de juego omg) \n>avatar (puedes cambiar el perfil con imagenes si es que discord no pueda restringir por el sobrecambio del perfil) \n \n-Música jijij: \n>play \n>leave```');
     }
 //ESPACIO:
     //comandos especiales y administracion
-    if (cmd === `${prefix}352421`) {
+    if (cmd === `${prefix}${code}`) {
         let status = args.join(' ');
         message.delete();
-        if (cmd === `${prefix}352421` && status === 'online') {
+        if (cmd === `${prefix}${code}` && status === 'online') {
             bot.user.setStatus(`Online`);
             message.delete();
             message.channel.send(`:white_check_mark: Status cambiado correctamente.`).then(msg => msg.delete(2000));
         }
-        if (cmd === `${prefix}352421` && status === 'idle') {
+        if (cmd === `${prefix}${code}` && status === 'idle') {
             bot.user.setStatus(`idle`);
             message.delete();
             message.channel.send(`:white_check_mark: Status cambiado correctamente.`).then(msg => msg.delete(2000));
         }
-        if (cmd === `${prefix}352421` && status === 'dnd') {
+        if (cmd === `${prefix}${code}` && status === 'dnd') {
             bot.user.setStatus(`dnd`);
             message.delete();
             message.channel.send(`:white_check_mark: Status cambiado correctamente.`).then(msg => msg.delete(2000));
         }
-        if (cmd === `${prefix}352421` && status === 'servers') {
+        if (cmd === `${prefix}${code}` && status === 'servers') {
             message.delete();
             message.channel.send(`estoi actualmente por ${bot.guilds.size} servidores o`);
         }
