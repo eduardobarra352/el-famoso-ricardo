377e2c62600feaa28bf604ece6af285c5068e5ad
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-20 19:41:16 -0300

    Update breaking.js

--- bp/breaking.js
+++ bp/breaking.js
@@ -1,5 +1,21 @@
 exports.run = async (bot, message, prefix, args, pdfcrowd, clientee) => {
       if (!args[0]) return message.reply("```>breakingnews [headline] | [ticker] | [imageurl]```");
+      try {
+                clientee.setOutputFormat("png");
+              } catch(why) {
+                console.error("Pdfcrowd Error: " + why);
+                console.error("Pdfcrowd Error Code: " + why.getCode());
+                console.error("Pdfcrowd Error Message: " + why.getMessage());
+                process.exit(1);
+              }
+              clientee.convertStringToFile(
+                `<script type="text/javascript" src="https://www.breakyourownnews.com/script.js?v=2.15"></script><link href="https://fonts.googleapis.com/css?family=Signika" rel="stylesheet"><canvas id="canvasOne" width="1280" height="720" class="byon-canvas">.</canvas>`,
+                "breakingnews.png",
+                function(err, fileName) {
+                  if (err) return console.error("Pdfcrowd Error: " + err);
+                  console.log("Success: the file was created " + fileName);
+                  message.channel.send({file: ("breakingnews.png")});
+                });
       let headline = args.join(' ');
       let ticker = args.join(' ');
       let imageurl = args.join(` `);
