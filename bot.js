0c3185774652e01cc4e120460204f04f69d7d9df
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-19 21:44:02 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -94,11 +94,11 @@ bot.on("message", async message => {
         if (!message.member.voiceChannel) return message.channel.send(":x: No estas en un canal de voz, porfa lentra™");
         if (message.guild.me.voiceChannel) return message.channel.send("uy pero ya estoi en elcanal de voz jej");
         if (!args[0]) return message.reply("```>play [url]```");
-        let validate = ytdl.validateURL(args[0]);
+        let validate = await ytdl.validateURL(args[0]);
         if (!validate) return message.reply(":no_entry: El Url es incorrecto o no es existente u.u");
-        let info = ytdl.getInfo(args[0]);
-        let connection = message.member.voiceChannel.join();
-        let dispatcher = connection.play(ytdl(args[0], { filter: "audioonly" }));
+        let info = await ytdl.getInfo(args[0]);
+        let connection = await message.member.voiceChannel.join();
+        let dispatcher = await connection.play(ytdl(args[0], { filter: "audioonly" }));
         message.channel.send(`:musical_note: Ahorita escuchando: ${info.title}`);
     }
     if (cmd === `${prefix}leave`) {
