2bd58ea57ad0c886853d7b16802f05d907229d40
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-19 22:21:39 -0300

    Update play.js

--- bp/play.js
+++ bp/play.js
@@ -8,7 +8,7 @@ exports.run = async (bot, message, args, opus) => {
         if (!validate) return message.reply(":no_entry: El Url es incorrecto o no es existente u.u");
         let info = await ytdl.getInfo(args[0]);
         let connection = await message.member.voiceChannel.join();
-        let dispatcher = await connection.play(ytdl(args[0], { filter: "audioonly" }));
+        let dispatcher = await connection.playStream(ytdl(args[0], { filter: "audioonly" }));
         message.channel.send(`:musical_note: Ahorita escuchando: ${info.title}`);
         if (!message.member.voiceChannel) return message.channel.send(":x: No estas en un canal de voz, porfa lentra™");
         if (!message.guild.me.voiceChannel) return message.reply(":no_entry: No puedo conectarme al tal canal de voz sorri");
