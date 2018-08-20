e700737877f9e05fd4993034a532355ee21c65dc
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-19 22:23:14 -0300

    Update play.js

--- bp/play.js
+++ bp/play.js
@@ -10,9 +10,4 @@ exports.run = async (bot, message, args, opus) => {
         let connection = await message.member.voiceChannel.join();
         let dispatcher = await connection.playStream(ytdl(args[0], { filter: "audioonly" }));
         message.channel.send(`:musical_note: Ahorita escuchando: ${info.title}`);
-        if (!message.member.voiceChannel) return message.channel.send(":x: No estas en un canal de voz, porfa lentra™");
-        if (!message.guild.me.voiceChannel) return message.reply(":no_entry: No puedo conectarme al tal canal de voz sorri");
-        if (message.guild.me.voiceChannelID !== message.member.voiceChannelID) return message.channel.send(":x: no estas en el canal de voz correcto,,,");
-        message.guild.me.voiceChannel.leave();
-        message.channel.send(":runner: Saliendo del canal de voz,,,").then(msg => msg.delete(2000));
 }
