d5a90779e7e2eefb3037c3228a93e6018527af37
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-21 00:10:51 -0300

    Update breaking.js

--- bp/breaking.js
+++ bp/breaking.js
@@ -3,7 +3,7 @@ exports.run = async (bot, message, cmd, prefix, args, pdfcrowd, clientee) => {
       let imagen = message.attachments.first().url;
       let barra = ' | ';
       let headline = args.join(' ').split(barra, ' ');
-      let ticker = args.slice().join(' ').split(headline, barra);
+      let ticker = args.slice().join(' ');
       if (!headline) return message.channel.send(":x: sedesconoce eltitulon, siga intentando");
       if (!ticker) return message.channel.send(":x: sedesconoce lainfo destacada, siga intentando");
       if (!imagen) return message.reply(":no_entry: soloesta permitido una imagen copiada o ya descargada");
