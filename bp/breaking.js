9fede6e2b3446ecf2a3de171437333880d22e146
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-21 17:03:38 -0300

    Update breaking.js

--- bp/breaking.js
+++ bp/breaking.js
@@ -2,8 +2,8 @@ exports.run = async (bot, message, cmd, prefix, args, pdfcrowd, clientee) => {
       if (!args[0]) return message.reply("```>breakingnews [headline] | [ticker] \n <imagen>```");
       let imagen = message.attachments.first().url;
       let barra = ' | ';
-      let headline = args.join(' ').split(' | ');
-      let ticker = args.slice(headline.lenght).join(' ');
+      let headline = args.join(' ').split(headline.lenght);
+      let ticker = args.slice(1).join(' ');
       if (!headline) return message.channel.send(":x: sedesconoce eltitulon, siga intentando");
       if (!ticker) return message.channel.send(":x: sedesconoce lainfo destacada, siga intentando");
       if (!imagen) return message.reply(":no_entry: soloesta permitido una imagen copiada o ya descargada");
@@ -13,7 +13,7 @@ exports.run = async (bot, message, cmd, prefix, args, pdfcrowd, clientee) => {
       if (m < 10) {
           m = "0" + m
       };
-          if (cmd === `${prefix}breakingnews` && headline && barra && ticker) {
+          if (cmd === `${prefix}breakingnews` + headline + barra + ticker) {
               message.channel.send(`:speech_balloon: Enviando,,,`).then(msg => msg.delete(4000));
               try {
                 clientee.setOutputFormat("png");
@@ -24,7 +24,7 @@ exports.run = async (bot, message, cmd, prefix, args, pdfcrowd, clientee) => {
                 process.exit(1);
               }
               clientee.convertStringToFile(
-                `<link href="https://fonts.googleapis.com/css?family=Signika" rel="stylesheet"/>\n<style type="text/css">\n.text2 {\nfont-family: Signika;\nfont-size: x-large;\ncolor: #FFFFFF;\ntext-align: center;\n}\n.auto-style1 {\nfont-family: Signika;\nfont-size: 30pt;}\n.auto-style2 {\nfont-family: Signika;\nfont-size: x-large;\ncolor: #000000;\ntext-align: left;\n}</style>\n<form id="form1" runat="server" style="width: 1024px; position: absolute; left: 0px; top: 0px; height: 546px;">\n<div style="height: 546px; position: absolute; left: 0px; top: 0px;"> \n<img height="546" src="https://github.com/eduardobarra352/el-famoso-ricardo/blob/master/img/breaking.png?raw=true" width="1024" />\n</div><img alt="" height="546" src=${imagen} width="1024" /></form>\n<p id="headli" class="auto-style1" style="position: absolute; left: 76px; top: 369px; width: 940px; height: 61px;">\n<strong>${headline}</strong></p>\n<p class="text2" style="position: absolute; left: 67px; top: 455px; width: 70px; height: 37px" id="clock">${h}:${m}</p>\n<p class="auto-style2" style="position: absolute; left: 159px; top: 456px; width: 852px; height: 30px" id="tick">\n<strong>${ticker}</strong></p>`,
+                `<link href="https://fonts.googleapis.com/css?family=Signika" rel="stylesheet"/><style type="text/css">.text2 {font-family: Signika;font-size: x-large;color: #FFFFFF;text-align: center;}.auto-style1 {font-family: Signika;font-size: 30pt;}.auto-style2 {font-family: Signika;font-size: x-large;color: #000000;text-align: left;}</style><form id="form1" runat="server" style="width: 1024px; position: absolute; left: 0px; top: 0px; height: 576px;"><div style="height: 576px; position: absolute; left: 0px; top: 0px;"> <img height="576" src="el-famoso-ricardo-master/img/breaking.png" width="1024" /></div><img alt="" height="576" src=${imagen} width="1024" /></form><p id="headli" class="auto-style1" style="position: absolute; left: 76px; top: 387px; width: 940px; height: 61px;"><strong>${headline}</strong></p><p class="text2" style="position: absolute; left: 69px; top: 480px; width: 70px; height: 37px" id="clock">${h}:${m}</p><p class="auto-style2" style="position: absolute; left: 161px; top: 480px; width: 852px; height: 30px" id="tick"><strong>${ticker}</strong></p>`,
                 "breakingnews.png",
                 function(err, fileName) {
                   if (err) return console.error("Pdfcrowd Error: " + err);
