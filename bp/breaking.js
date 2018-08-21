5f333083d34c64bc845a37c6c7e43096bcb04e60
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-20 22:13:41 -0300

    Update breaking.js

--- bp/breaking.js
+++ bp/breaking.js
@@ -1,19 +1,18 @@
 exports.run = async (bot, message, cmd, prefix, args, pdfcrowd, clientee) => {
-      if (!args[0]) return message.reply("```>breakingnews [headline] [ticker] [imageurl]```");
-      let img = new Image();
+      if (!args[0]) return message.reply("```>breakingnews [headline] [ticker] \n <imagen>```");
+      let imagen = message.attachments.first().url;
       let headline = args.join(' ');
       let ticker = args.join(' ');
-      let imageurl = args.join(` `);
       if (!headline) return message.channel.send(":x: sedesconoce eltitulon, siga intentando");
       if (!ticker) return message.channel.send(":x: sedesconoce lainfo destacada, siga intentando");
-      if (!imageurl) return message.channel.send(":x: no sepuede sin el url dela imagen, siga intentando");
+      if (!imagen) return message.reply(":no_entry: soloesta permitido una imagen copiada o ya descargada");
       const today = new Date();
       var m = today.getMinutes();
       var h = today.getHours();
       if (m < 10) {
           m = "0" + m
       };
-          if (cmd === `${prefix}breakingnews` + headline + ticker + imageurl === img.url) {
+          if (cmd === `${prefix}breakingnews` + headline + ticker) {
               message.channel.send(`:speech_balloon: Enviando,,,`).then(msg => msg.delete(4000));
               try {
                 clientee.setOutputFormat("png");
@@ -24,7 +23,7 @@ exports.run = async (bot, message, cmd, prefix, args, pdfcrowd, clientee) => {
                 process.exit(1);
               }
               clientee.convertStringToFile(
-                `<link href="https://fonts.googleapis.com/css?family=Signika" rel="stylesheet"/><style type="text/css">.text2 {font-family: Signika;font-size: x-large;color: #FFFFFF;text-align: center;}.auto-style1 {font-family: Signika;font-size: 30pt;}.auto-style2 {font-family: Signika;font-size: x-large;color: #000000;text-align: left;}</style><body><form id="form1" runat="server" style="width: 1024px; position: absolute; left: 4px; top: 5px; height: 546px;"><div style="height: 576px; position: absolute; left: -7px; top: -13px;"> <img height="546" src="https://github.com/eduardobarra352/el-famoso-ricardo/blob/master/img/breaking.png?raw=true" width="1024" /></div><img alt="" height="546" src=${imageurl} width="1024" /></form><p id="headline" class="auto-style1" style="position: absolute; left: 73px; top: 358px; width: 940px; height: 61px;"><strong>${headline}</strong></p><p class="text2" style="position: absolute; left: 67px; top: 446px; width: 70px; height: 37px" id="clock">${h}:${m}</p><p class="auto-style2" style="position: absolute; left: 159px; top: 445px; width: 852px; height: 30px" id="ticker"><strong>${ticker}</strong></p></body>`,
+                `<link href="https://fonts.googleapis.com/css?family=Signika" rel="stylesheet"/><style type="text/css">.text2 {font-family: Signika;font-size: x-large;color: #FFFFFF;text-align: center;}.auto-style1 {font-family: Signika;font-size: 30pt;}.auto-style2 {font-family: Signika;font-size: x-large;color: #000000;text-align: left;}</style><body><form id="form1" runat="server" style="width: 1024px; position: absolute; left: 4px; top: 5px; height: 546px;"><div style="height: 576px; position: absolute; left: -7px; top: -13px;"> <img height="546" src="https://github.com/eduardobarra352/el-famoso-ricardo/blob/master/img/breaking.png?raw=true" width="1024" /></div><img alt="" height="546" src=${imagen} width="1024" /></form><p id="headline" class="auto-style1" style="position: absolute; left: 73px; top: 358px; width: 940px; height: 61px;"><strong>${headline}</strong></p><p class="text2" style="position: absolute; left: 67px; top: 446px; width: 70px; height: 37px" id="clock">${h}:${m}</p><p class="auto-style2" style="position: absolute; left: 159px; top: 445px; width: 852px; height: 30px" id="ticker"><strong>${ticker}</strong></p></body>`,
                 "breakingnews.png",
                 function(err, fileName) {
                   if (err) return console.error("Pdfcrowd Error: " + err);
