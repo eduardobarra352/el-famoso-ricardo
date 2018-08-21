c508f91400047c91a42e405bcf357ecbf1a46b50
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-21 19:50:53 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -153,7 +153,7 @@ bot.on("message", message => {
               }
           };
           message.channel.send(`:speech_balloon: Enviando,,,`).then(msg => msg.delete(1000));
-          webshot(`<style type="text/css">\n.auto-style1 {\n	text-align: center;\n	font-size: 35pt;\n}\n</style>\n<body style="width: 1024px; height: 798px;">\n<p id="textu" class="auto-style1" enableviewstate="true" style="position: absolute; left: 347px; top: 335px; width: 563px; height: 246px; max-width: 267px; max-height: 125px; line-height: normal; vertical-align: text-top; color: #000000; font-style: normal; word-spacing: 20px;" visible="true">${tus}</p><p style="width: 504px">\n<img alt="image" height="801" src="https://cdn.discordapp.com/attachments/415365025121697792/478043122169937920/tus.png" width="1024" /></p>\n</body>`, 'Tu.png', options, {siteType:'html'}, function(err) {
+          webshot(`<style type="text/css">\n.auto-style1 {\n	text-align: center;\n	font-size: 35pt;\n}\n</style>\n<body style="width: 1024px; height: 798px;">\n<p id="textu" class="auto-style1" enableviewstate="true" style="position: absolute; left: 347px; top: 335px; width: 563px; height: 246px; max-width: 267px; max-height: 125px; line-height: normal; vertical-align: text-top; color: #000000; font-style: normal; word-spacing: 20px;" visible="true">${tus}</p><p style="width: 504px">\n<img alt="image" height="801" src="https://cdn.discordapp.com/attachments/415365025121697792/478043122169937920/tus.png" width="1024" /></p>\n</body>`, "Tu.png", options, {siteType:'html'}, function(err) {
               message.channel.send({file: ("Tu.png")});
           });
           //clientee.convertStringToFile(
