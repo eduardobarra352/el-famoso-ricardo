0c4a14922448859d683df680439f747ab3d43bb5
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-21 19:27:29 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,8 @@ const ytdl = require('ytdl-core');
 const opus = require('node-opus');
 const opusscript = require('opusscript');
 const ffmpegbinaries = require('ffmpeg-binaries');
-const html2png = require('html2png');
+const imagify = require('htmlshot');
+var data = ' data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAD4SURBVHjaYrm7igEd/IfSjMiCTH/+MDAohd4FYyC7gV8znwGEQWwkcQam378RuoDsemGdPAYQBrGRxBlYfv1i2H9+jrIDSEBSP5GB4ddTsKSwZiIDUBzmjAOMx6cw/LfIvsvA8HE/A1bA78hwYqoyA+OBXgaQafsVdCwY5HXMUdQ8vHKS4cGVEyCmI8vPnwwHgIzG149u1MsrcaIoBIoxAOUbQVaD3AgC9opKLAwM328xvH39E+JGUXYGkNiLpwz2ID4L1NcOwvxvGG5cYmC4fh1sA4OmJoODhgbYx2CPMq4qBysE+QQkALKmAWoziAYFEUijI0CAAQDkpFzX1sj/9wAAAABJRU5ErkJggg==';
 const avconv = require('avconv');
 const active = new Map();
 
@@ -147,10 +148,9 @@ bot.on("message", message => {
         if (!args[0]) return message.reply("```>tu [texto]```");
         if (cmd === `${prefix}tu` && tus) {
           message.channel.send(`:speech_balloon: Enviando,,,`).then(msg => msg.delete(2000));
-            var screenshot = html2png({ width: 1024, height: 798, browser: 'chromedriver'});
-            screenshot.render('<style type="text/css">\n.auto-style1 {\n	text-align: center;\n	font-size: 35pt;\n}\n</style>\n<body style="width: 1024px; height: 798px;">\n<p id="textu" class="auto-style1" enableviewstate="true" style="position: absolute; left: 347px; top: 335px; width: 563px; height: 246px; max-width: 267px; max-height: 125px; line-height: normal; vertical-align: text-top; color: #000000; font-style: normal; word-spacing: 20px;" visible="true">${tus}</p><p style="width: 504px">\n<img alt="image" height="801" src="https://cdn.discordapp.com/attachments/415365025121697792/478043122169937920/tus.png" width="1024" /></p>\n</body>', function (err, data) {
-                if (err) return screenshot.error(err, cb);
-                console.log(data);
+            imagify(data,"Tu");
+            var test1=imagify(d.body,(base64)=>{
+                $('body').append(`<style type="text/css">\n.auto-style1 {\n	text-align: center;\n	font-size: 35pt;\n}\n</style>\n<body style="width: 1024px; height: 798px;">\n<p id="textu" class="auto-style1" enableviewstate="true" style="position: absolute; left: 347px; top: 335px; width: 563px; height: 246px; max-width: 267px; max-height: 125px; line-height: normal; vertical-align: text-top; color: #000000; font-style: normal; word-spacing: 20px;" visible="true">${tus}</p><p style="width: 504px">\n<img alt="image" height="801" src="https://cdn.discordapp.com/attachments/415365025121697792/478043122169937920/tus.png" width="1024" /></p>\n</body>`);
                 message.channel.send({file: ("Tu.png")});
             });
           //clientee.convertStringToFile(
