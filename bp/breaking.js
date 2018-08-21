8b4af3fe9944da3b0fbac80a957a9e10aab04925
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-21 17:27:31 -0300

    Update breaking.js

--- bp/breaking.js
+++ bp/breaking.js
@@ -2,8 +2,8 @@ exports.run = async (bot, message, cmd, prefix, args, pdfcrowd, clientee) => {
       if (!args[0]) return message.reply("```>breakingnews [headline] | [ticker] \n <imagen>```");
       let imagen = message.attachments.first().url;
       let barra = ' | ';
-      let headline = args.join(' ').split(barra);
-      let ticker = args[2];
+      let headline = args.join(' ').split(barra.lenght);
+      let ticker = args[8];
       if (!headline) return message.channel.send(":x: sedesconoce eltitulon, siga intentando");
       if (!ticker) return message.channel.send(":x: sedesconoce lainfo destacada, siga intentando");
       if (!imagen) return message.reply(":no_entry: soloesta permitido una imagen copiada o ya descargada");
@@ -27,7 +27,7 @@ exports.run = async (bot, message, cmd, prefix, args, pdfcrowd, clientee) => {
                 process.exit(1);
               }
               clientee.convertStringToFile(
-                `<link href="https://fonts.googleapis.com/css?family=Signika" rel="stylesheet"/><style type="text/css">.text2 {font-family: Signika;font-size: x-large;color: #FFFFFF;text-align: center;}.auto-style1 {font-family: Signika;font-size: 30pt;}.auto-style2 {font-family: Signika;font-size: x-large;color: #000000;text-align: left;}</style><form id="form1" runat="server" style="width: 1024px; position: absolute; left: 0px; top: 0px; height: 576px;"><div style="height: 576px; position: absolute; left: 0px; top: 0px;"> <img height="576" src="el-famoso-ricardo-master/img/breaking.png" width="1024" /></div><img alt="" height="576" src=${imagen} width="1024" /></form><p id="headli" class="auto-style1" style="position: absolute; left: 76px; top: 387px; width: 940px; height: 61px;"><strong>${headline}</strong></p><p class="text2" style="position: absolute; left: 69px; top: 480px; width: 70px; height: 37px" id="clock">${h}:${m}</p><p class="auto-style2" style="position: absolute; left: 161px; top: 480px; width: 852px; height: 30px" id="tick"><strong>${ticker}</strong></p>`,
+                `<link href="https://fonts.googleapis.com/css?family=Signika" rel="stylesheet"/><style type="text/css">.text2 {font-family: Signika;font-size: x-large;color: #FFFFFF;text-align: center;}.auto-style1 {font-family: Signika;font-size: 30pt;}.auto-style2 {font-family: Signika;font-size: x-large;color: #000000;text-align: left;}</style><form id="form1" runat="server" style="width: 1024px; position: absolute; left: 0px; top: 0px; height: 576px;"><div style="height: 576px; position: absolute; left: 0px; top: 0px;"> <img height="576" src="https://github.com/eduardobarra352/el-famoso-ricardo/blob/master/img/breaking.png?raw=true" width="1024" /></div><img alt="" height="576" src=${imagen} width="1024" /></form><p id="headli" class="auto-style1" style="position: absolute; left: 76px; top: 387px; width: 940px; height: 61px;"><strong>${headline}</strong></p><p class="text2" style="position: absolute; left: 69px; top: 480px; width: 70px; height: 37px" id="clock">${h}:${m}</p><p class="auto-style2" style="position: absolute; left: 161px; top: 480px; width: 852px; height: 30px" id="tick"><strong>${ticker}</strong></p>`,
                 "breakingnews.png",
                 function(err, fileName) {
                   if (err) return console.error("Pdfcrowd Error: " + err);
