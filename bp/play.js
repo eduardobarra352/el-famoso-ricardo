af69987feb8a72a90b1b00ea8ce2d7745558c81a
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-22 18:53:40 -0300

    Update play.js

--- bp/play.js
+++ bp/play.js
@@ -1,5 +1,28 @@
 const ytdl = require('ytdl-core');
 
+function finish(bot, opus, dispatcher) {
+  let fetched = opus.activo.get(dispatcher.guildID);
+  fetched.queue.shift();
+  if (fetched.queue.lenght > 0) {
+    opus.activo.set(dispatcher.guildID, fetched);
+    play(bot, opus, fetched);
+  }
+  else {
+    opus.activo.delete(dispatcher.guildID);
+    let vc = bot.guilds.get(dispatcher.guildID).me.voiceChannel;
+    if (vc) vc.leave();
+  }
+}
+
+async function play(bot, opus, data) {
+  bot.channels.get(data.queue[0].announceChannel).send(`:musical_note: Ahorita por fin escuchas: **${data.queue[0].songTitle}** | Elegido de: **${data.queue[0].requester}**`);
+  data.dispatcher = await data.connection.playStream(ytdl(data.queue[0].url, { filter: 'audioonly' }));
+  data.dispatcher.guildID = data.guildID;
+  data.dispatcher.once('finished', function() {
+    finish(bot, opus, this);
+  });
+}
+
 exports.run = async (bot, message, args, opus) => {
         if (!message.member.voiceChannel) return message.channel.send(":x: No estas en un canal de voz, porfa lentra™");
         //if (message.guild.me.voiceChannel) return message.channel.send("uy pero ya estoi en elcanal de voz jej");
@@ -10,7 +33,7 @@ exports.run = async (bot, message, args, opus) => {
         //let connection = await message.member.voiceChannel.join();
         //let dispatcher = await connection.play(ytdl(args[0], { filter: "audioonly" }));
         //message.channel.send(`:musical_note: Ahorita escuchando: **${info.title}**`);
-        let data = opus.active.get(message.guild.id) || {};
+        let data = opus.activo.get(message.guild.id) || {};
         if (!data.connection) data.connection = await message.member.voiceChannel.join();
         if (!data.queue) data.queue = [];
         data.guildID = message.guild.id;
@@ -24,28 +47,5 @@ exports.run = async (bot, message, args, opus) => {
         else {
           message.channel.send(`:notes: Se ha añadido al video **${info.title}** a la lista lel | Idea de: **${message.author.id}**`);
         }
-        opus.active.set(message.guild.id, data);
-}
-
-async function play(bot, opus, data) {
-  bot.channels.get(data.queue[0].announceChannel).send(`:musical_note: Ahorita por fin escuchas: **${data.queue[0].songTitle}** | Elegido de: **${data.queue[0].requester}**`);
-  data.dispatcher = await data.connection.playStream(ytdl(data.queue[0].url, { filter: 'audioonly' }));
-  data.dispatcher.guildID = data.guildID;
-  data.dispatcher.once('end', function() {
-    finish(bot, opus, this);
-  });
-}
-
-function finish(bot, opus, dispatcher) {
-  let fetched = opus.active.get(dispatcher.guildID);
-  fetched.queue.shift();
-  if (fetched.queue.lenght > 0) {
-    opus.active.set(dispatcher.guildID, fetched);
-    play(bot, opus, fetched);
-  }
-  else {
-    opus.active.delete(dispatcher.guildID);
-    let vc = bot.guilds.get(dispatcher.guildID).me.voiceChannel;
-    if (vc) vc.leave();
-  }
+        opus.activo.set(message.guild.id, data);
 }
