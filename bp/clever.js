1567b8d3d6c961443bba7d20f4cc9f614c9e2eba
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-23 19:04:34 -0300

    Update clever.js

--- bp/clever.js
+++ bp/clever.js
@@ -5,10 +5,11 @@ exports.run = async (bot, message, args) => {
  let input = args.join(' ');
  if (!input) return message.reply("```>di [intenta hablarle owo]```");
  let clbt = new Cleverbot('3hvWzi38KHKHHY0y', 'vdAjTZR6qfnxKmJOX9cSotqjD5K1qzxK');
- clbt.setNick('ricardo');
+ clbt.setNick('Ricardo');
  clbt.create(function (err, session) {
    clbt.ask(input, function (err, response) {
          console.log(session + ':', response)
+         message.channel.send(`:speech_balloon: Respondiendo,,,`).then(msg => msg.delete(5000));
          message.channel.send(response);
        });
    });
