1397243f4e04ebe65f69702726af5900c06e3160
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-23 18:44:01 -0300

    Add files via upload

---
+++ bp/clever.js
@@ -0,0 +1,14 @@
+const Cleverbot = require('cleverbot.io');
+
+exports.run = async (bot, message, args) {
+  'use strict';
+ let input = process.args.slice(2).join(' ');
+ let clbt = new Cleverbot('3hvWzi38KHKHHY0y', 'vdAjTZR6qfnxKmJOX9cSotqjD5K1qzxK');
+ clbt.setNick('Vlad');
+ clbt.create(function (err, session) {
+   clbt.ask(input, function (err, response) {
+         console.log(session + ':', response)
+         message.channel.send(session + ':', response);
+       });
+   });
+}
