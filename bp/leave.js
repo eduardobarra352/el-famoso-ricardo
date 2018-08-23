fbef05f9f5affc6de6b302c59a623663b5fa89fc
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-23 20:15:48 -0300

    Update leave.js

--- bp/leave.js
+++ bp/leave.js
@@ -6,4 +6,5 @@ exports.run = async (bot, message, args, opus) => {
         if (message.guild.me.voiceChannelID !== message.member.voiceChannelID) return message.channel.send(":x: no estas en el canal de voz correcto,,,");
         message.guild.me.voiceChannel.leave();
         message.channel.send(":runner: Saliendo del canal de voz,,,").then(msg => msg.delete(2000));
+        console.log(`>leave usado por: ${message.author.tag} en el server ${message.guild.name}`);
 }
