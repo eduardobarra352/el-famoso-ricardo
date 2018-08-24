e27bfeeefafad3f70318c053b30067ab8270b3c4
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-08-24 17:42:03 -0300

    Update breaking.js

--- bp/breaking.js
+++ bp/breaking.js
@@ -33,6 +33,7 @@ exports.run = async (bot, message, cmd, prefix, args, webshot, Log) => {
             };
             webshot(`<link href="https://fonts.googleapis.com/css?family=Signika" rel="stylesheet"/><style type="text/css">.text2 {	font-family: Signika;	font-size: x-large;	color: #FFFFFF;	text-align: center;}.auto-style1 {	font-family: Signika;	font-size: 30pt;}.auto-style2 {	font-family: Signika;	font-size: x-large;	color: #000000;	text-align: left;}.auto-style3 {	margin-bottom: 0px;}</style><form id="form1" runat="server" style="width: 1024px; position: absolute; left: 0px; top: 0px; height: 576px;" class="auto-style3"><div style="height: 576px; position: absolute; left: 0px; top: 0px;"> <img height="576" src="https://github.com/eduardobarra352/el-famoso-ricardo/blob/master/img/breaking.png?raw=true" width="1024" /></div><img alt="" height="576" src=${imagen} width="1024" /></form><p id="headli" class="auto-style1" style="position: absolute; left: 76px; top: 387px; width: 9000px; height: 61px;"><strong>${headline}</strong></p><p class="text2" style="position: absolute; left: 69px; top: 480px; width: 70px; height: 37px" id="clock">${h}:${m}</p><p class="auto-style2" style="position: absolute; left: 161px; top: 480px; width: 9000px; height: 30px" id="tick"><strong>${ticker}</strong></p>`, 'breakingnews.png', options, function(err) {
             message.channel.send({file: ("breakingnews.png")});
+            message.channel.stopTyping();
             });
       }
   }
