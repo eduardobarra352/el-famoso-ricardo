c1947633d2d61690e5efb681f8e6b20a27fdc5d9
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-09-02 16:01:18 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -219,7 +219,7 @@ bot.on("message", message => {
                   height: 798
               }
           };
-          webshot(`<style type="text/css">\n.auto-style1 {\n	text-align: center;\n	font-size: 35pt;\n}\n</style>\n<body style="width: 1024px; height: 798px;">\n<p id="textu" class="auto-style1" enableviewstate="true" style="position: absolute; left: 347px; top: 335px; width: 563px; height: 246px; max-width: 267px; max-height: 125px; line-height: normal; vertical-align: text-top; color: #000000; font-style: normal; word-spacing: 20px;" visible="true">${tus}</p><p style="width: 504px">\n<img alt="image" height="801" src=${imgtus} width="1024" /></p>\n</body>`, 'Tu.png', options, function(err) {
+          webshot(`<style type="text/css">\n.auto-style1 {\n	text-align: center;\n	font-size: 35pt;\n}\n</style>\n<body style="width: 1024px; height: 798px;">\n<p id="textu" class="auto-style1" enableviewstate="true" style="position: absolute; left: 347px; top: 335px; width: 563px; height: 246px; max-width: 267px; max-height: 125px; line-height: normal; vertical-align: text-top; color: #000000; font-style: normal; word-spacing: 20px;" visible="true">${tus}</p><p style="width: 504px">\n<img alt="image" height="801" src=${imgtus.file} width="1024" /></p>\n</body>`, 'Tu.png', options, function(err) {
               message.channel.send({file: ("Tu.png")});
               message.channel.stopTyping();
           });
