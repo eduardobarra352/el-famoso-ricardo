814d94688e8e9ba7af9b42b85d2df2e6d3d123b6
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2018-09-03 18:08:39 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -256,7 +256,7 @@ bot.on("message", message => {
       if (!args[0]) return message.reply("```>john [textoXD]```");
       if (cmd === `${prefix}john` && jon) {
         if (args[4]) {
-            jon = args.join(' ').split(barra);
+            jon = args.slice(jon.lenght).split(barra);
         }
         message.channel.startTyping();
         console.log(`${prefix}john usado por: ${message.author.tag} en el server ${message.guild.name} con su uso "${jon}"`);
