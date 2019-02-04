4111422364b77b2ba67dbb1a16ffd9e815c56f3f
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2019-02-04 19:22:34 -0300

    Update desmotivacion.js

--- bp/desmotivacion.js
+++ bp/desmotivacion.js
@@ -2,9 +2,10 @@ exports.run = async (bot, message, cmd, prefix, args, webshot, Log) => {
   let barra = ' | ';
   let urlimagen = args[0];
   //let imagen = message.attachments.first().url;
-  let args2 = message.content.slice(args.lenght).trim().split(barra);
-  let toptext = args[1];
-  let bottomtext = args2[4];
+  let args2 = message.content.slice(cmd.lenght).trim().split(barra);
+  let args3 = message.content.slice(args2.lenght).trim().split(barra);
+  let toptext = args2[1];
+  let bottomtext = args3[2];
   if (!args[0]) return message.reply("```1- >desmotivacion [url] [toptext] \n2- >desmotivacion [url] [toptext] | [bottomtext] \n3- >desmotivacion [toptext] | [bottomtext] \n <imagen>```");
   if (!toptext) return console.log(`${prefix}desmotivacion usado por: ${message.author.tag} en el server ${message.guild.name} con falta de usos`);
   if (cmd === `${prefix}desmotivacion` && urlimagen && toptext) {
