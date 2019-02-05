41eb1e02e51af05bb5343adb5bd437c1506fc405
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2019-02-05 15:30:25 -0300

    Update desmotivacion.js

--- bp/desmotivacion.js
+++ bp/desmotivacion.js
@@ -1,7 +1,7 @@
 exports.run = async (bot, message, cmd, prefix, args, webshot, Log) => {
   let barra = ' | ';
   let urlimagen = args[0];
-  let espaciador = [' ', ' | ';
+  let espaciador = [' ', ' | '];
   let args2 = message.content.slice(cmd.lenght).trim().split(espaciador);
   let args3 = message.content.slice(args2.lenght).trim().split(barra);
   let toptext = args2[1];
