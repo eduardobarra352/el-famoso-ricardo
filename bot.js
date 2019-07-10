ab795ad9dd8552498aaa1743cedce8364d6ea7a2
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2019-07-09 21:34:13 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -227,14 +227,14 @@ bot.on("message", message => {
           }
     }
     if (cmd === `${prefix}desmotivacion`) {
-      //let barra = ' | ';
+      let barra = ' | ';
       let urlimagen = args[0];
       let space = urlimagen + ' ';
       //let urlimagen2 = args[0];
       let args2 = message.content.slice(cmd.lenght).trim().split(space);
-      //let args3 = message.content.slice(args2.lenght).trim().split(barra);
+      let args3 = message.content.slice(args2.lenght).trim().split(barra);
       let toptext = args2[1];
-      /*let bottomtext = args3[2];
+      let bottomtext = args3[2];/*
       let toptext2 = args2[1];
       let bottomtext2 = args3[2];
       let toptext3 = args[0];
@@ -252,7 +252,7 @@ bot.on("message", message => {
                 height: 598
           }
       };
-      webshot(`<head><style type="text/css">.auto-style1 {	color: #FFFFFF;	text-align: center;	font-size: 28pt;	margin-bottom: 0px;}.auto-style2 {	color: #FFFFFF;	text-align: center;	font-size: large;	margin-bottom: 0px;}</style></head> <form id="form1" runat="server" style="width: 487px; position: absolute; left: 0px; top: 0px; height: 602px;" class="auto-style2">	<div style="height: 253px; position: absolute; left: 0px; top: 0px; width: 113px;"> 		<img height="598" src="https://raw.githubusercontent.com/eduardobarra352/el-famoso-ricardo/master/img/desmotivacion.png" width="650" />	</div><img alt="" height="541" src=${urlimagen} width="647" /></form> <p class="auto-style1" style="width: 587px; height: 22px; position: absolute; left: 32px; top: 464px;">${toptext}</p><p class="auto-style2" style="width: 587px; height: 87px; position: absolute; left: 32px; top: 529px;"></p>`, 'desmotivacion.png', options, function(err) {
+      webshot(`<head><style type="text/css">.auto-style1 {	color: #FFFFFF;	text-align: center;	font-size: 28pt;	margin-bottom: 0px;}.auto-style2 {	color: #FFFFFF;	text-align: center;	font-size: large;	margin-bottom: 0px;}</style></head> <form id="form1" runat="server" style="width: 487px; position: absolute; left: 0px; top: 0px; height: 602px;" class="auto-style2">	<div style="height: 253px; position: absolute; left: 0px; top: 0px; width: 113px;"> 		<img height="598" src="https://raw.githubusercontent.com/eduardobarra352/el-famoso-ricardo/master/img/desmotivacion.png" width="650" />	</div><img alt="" height="541" src=${urlimagen} width="647" /></form> <p class="auto-style1" style="width: 587px; height: 22px; position: absolute; left: 32px; top: 464px;">${toptext}</p><p class="auto-style2" style="width: 587px; height: 87px; position: absolute; left: 32px; top: 529px;">${bottomtext}</p>`, 'desmotivacion.png', options, function(err) {
       message.channel.send({file: ("desmotivacion.png")});
       message.channel.stopTyping();
       });
