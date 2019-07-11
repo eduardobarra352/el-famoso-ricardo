405645b0306d8da4e54f10b42257f48f98e9b291
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2019-07-10 20:56:49 -0400

    posiblemente lo hice

--- bot.js
+++ bot.js
@@ -173,14 +173,25 @@ bot.on("message", message => {
                     .setImage(res[i].url);
                     if (minim == 0) { message.channel.send(embed).then(msg => msgid = msg); }
                 }
-                const filtro = m => ["next", "sig"].includes(m.content);
+                //avanzar
+                const filtro = m => minim > 0 && nivel < 10 ["next", "sig"].includes(m.content);
                 const collector = message.channel.createMessageCollector(filtro, { time: 30000 });
                 collector.res = res;
                 collector.once('collect', function(m) {
                     nivel += 1;
                     minim += 1;
                     gis(buscar, resultados);
-                    setTimeout(()=>{ msgid.edit(embed); },2000);
+                    setTimeout(()=>{ msgid.edit(embed); },500);
+                });
+                //retroceder
+                const filtro2 = m => minim < 9 && nivel > 1 ["prev", "atras"].includes(m.content);
+                const collector2 = message.channel.createMessageCollector(filtro2, { time: 30000 });
+                collector2.res = res;
+                collector2.once('collect', function(m) {
+                    nivel -= 1;
+                    minim -= 1;
+                    gis(buscar, resultados);
+                    setTimeout(()=>{ msgid.edit(embed); },500);
                 });
             }
             catch(e) {
