ccf980c5332f69c1e91bbe7005f4609d8bb715a6
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2019-07-12 15:33:50 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -290,7 +290,7 @@ bot.on("message", message => {
 	let imagen;
 	let imagensize;
 	message.channel.fetchMessages({ limit: 10 })
-  	.then(messages => console.log((messages.attachments).array()))
+  	.then(messages => { var Attachment = messages.array(); console.log(Attachment); })
   	.catch(console.error);
 	//message.channel.send({ files: [imagen] });
     }
