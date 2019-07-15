f3c1eef651764e79fe049421ef7dd967957ecf86
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2019-07-14 22:56:41 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,6 @@ bot.on("ready", () => {
     //setInterval(function(){
 	request('http://barrarchiverio.7m.pl/a/access?client_id='+process.env.daclid+'&client_secret='+process.env.daclisecret, function (err, response, body) {
 		body = JSON.stringify(body, null, '  ');
-		body = JSON.parse(body);
 		console.log(body.Respuesta);
 	});
     //}, 3600000);
