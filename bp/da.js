425f4a63b214033130f1e0892e1072178f97013d
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2019-07-15 19:35:47 -0400

    Update da.js

--- bp/da.js
+++ bp/da.js
@@ -156,7 +156,7 @@ exports.run = async (bot, message, args, AttachImagen) => {
         if (e) { message.channel.send(":x: Uy, un erroralgo feo, mmmm siga intentando"); message.channel.stopTyping(); return; }
         r = JSON.stringify(b, null, '  ');
         r = JSON.parse(r);
-        console.log(r);
+        console.log(r.stats);
         limite = r.stats.user_deviations;
       }});
       message.channel.startTyping();
