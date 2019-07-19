8c5b484845a04232582e849d2909ef7564bdd575
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2019-07-19 01:43:49 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -603,7 +603,7 @@ bot.on("message", message => {
         }
 	if (args[0].includes('emojis')) {
             message.delete();
-	    let em = bot.emojis.map.slice(0, 10);
+	    let em = bot.emojis.map().slice(0, 10);
 	    let mens = 'emojis: ${bot.emojis.size}: ';
             for(var i in em) {
             	mens += `<:${em[i].map(e=>e.name)}:${em[i].map(e=>e.id)}>`;
