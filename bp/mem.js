72788dff5c52968e98ca0ad21440d42d98c6d282
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2019-07-21 02:58:52 -0400

    Update mem.js

--- bp/mem.js
+++ bp/mem.js
@@ -1,3 +1,23 @@
+const meme = require('nodejs-meme-generator');
+
 exports.run = async (bot, message, args, AttachImagen) => {
-  
+  let memeGenerator = new meme({
+    canvasOptions: {
+      canvasWidth: 500,
+      canvasHeight: 500
+    },
+    fontOptions: {
+      fontSize: 46,
+      fontFamily: 'impact',
+      lineHeight: 2
+    }
+  });
+  memeGenerator.generateMeme({
+    topText: 'Meme',
+    bottomText: 'Generator',
+    url: 'https://i.imgur.com/7FHoSIG.png'
+   })
+   .then(function(data) {
+    console.log(data);
+   });
 }
