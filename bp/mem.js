7c707c248d20bfc5465d9c59b96a6efbd630c78a
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2019-07-21 14:31:27 -0400

    Update mem.js

--- bp/mem.js
+++ bp/mem.js
@@ -4,7 +4,7 @@ exports.run = async (bot, message, args, AttachImagen) => {
   if (!args[0] && !message.attachments.size) return message.reply("```>meme [url] [tExTo] \n>meme [TeXtO] <imagen>```");
   if (message.attachments.size > 0) {
     if (!args[0]) return message.reply(":x: nohay ninguntexto aparteXD sigale,,,,");
-    let barra = ' | ';
+    let barra = '|';
     let indexBarra = args.indexOf(barra);
     console.log(indexBarra);
     let toptext = args.slice(0, indexBarra).join(' ');
