573643290bcd83b18cea766698b9765e9956ae06
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2019-07-21 03:44:24 -0400

    Update mem.js

--- bp/mem.js
+++ bp/mem.js
@@ -1,32 +1,36 @@
 const meme = require('nodejs-meme-generator');
-const http = require('http');
-const fs = require('fs');
 
 exports.run = async (bot, message, args, AttachImagen) => {
-  let memeGenerator = new meme({
-    canvasOptions: {
-      canvasWidth: 500,
-      canvasHeight: 500
-    },
-    fontOptions: {
-      fontSize: 46,
-      fontFamily: 'arial',
-      lineHeight: 3
-    }
-  });
-  memeGenerator.generateMeme({
-    topText: 'Meme',
-    bottomText: 'Generator',
-    url: 'https://i.imgur.com/7FHoSIG.png'
-   })
-   .then(function(data) {
-    fs.readFile(data, function(err, mem) {
-      if (err) { message.channel.send(":x: Uy, un erroralgo feo, mmmm siga intentando"); console.log(err); }
-      http.createServer(function(req, res) {
-        res.writeHead(200, {'Content-Type': 'image/png'});
-        res.end(mem);
-        message.channel.send({file: (mem) });
-      })
+  if (!args[0] && !message.attachments.size) return message.reply("```>meme [url] [tExTo] \n>meme [TeXtO] <imagen>```");
+  if (message.attachments.size > 0) {
+    if (!args[0]) return message.reply(":x: nohay ninguntexto aparteXD sigale,,,,");
+    let barra = [' | ', '|'];
+    let toptext = args.join(' ').slice(0, barra);
+    let bottomtext = args.slice(barra);
+    let mitad = Math.ceil(toptext.length / 2);
+    if (bottomtext == undefined) bottomtext = args.slice(mitad);
+    message.channel.startTyping();
+    let imagen = message.attachments.first().url;
+    let anchura = message.attachments.first().width;
+    let altura = message.attachments.first().height;
+    let opt = new meme({
+      canvasOptions: {
+        canvasWidth: anchura,
+        canvasHeight: altura
+      },
+      fontOptions: {
+        fontSize: 46,
+        fontFamily: 'arial',
+        lineHeight: 3
+      }
     });
-   });
+    opt.generateMeme({
+      topText: toptext,
+      bottomText: bottomtext,
+      url: imagen
+     })
+     .then(function(data) {
+      message.channel.send({file:(data)});
+     });
+  }
 }
