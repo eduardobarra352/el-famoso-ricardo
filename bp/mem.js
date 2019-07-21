26d162c5d80657147e4450cb6d716b897461176e
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2019-07-21 14:33:46 -0400

    Update mem.js

--- bp/mem.js
+++ bp/mem.js
@@ -8,7 +8,7 @@ exports.run = async (bot, message, args, AttachImagen) => {
     let indexBarra = args.indexOf(barra);
     console.log(indexBarra);
     let toptext = args.slice(0, indexBarra).join(' ');
-    let bottomtext = args.slice(indexBarra).join(' ');
+    let bottomtext = args.slice(indexBarra+1).join(' ');
     if (indexBarra) args.splice(indexBarra);
     if (bottomtext == undefined && args > 0) {
       toptext = args.slice(0, Math.ceil(args.length / 2)).join(' ');
