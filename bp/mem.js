d7e0e4b23c55681e9860b50c7aac8da1c28fd3cf
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2019-07-21 03:58:59 -0400

    Update mem.js

--- bp/mem.js
+++ bp/mem.js
@@ -4,17 +4,17 @@ exports.run = async (bot, message, args, AttachImagen) => {
   if (!args[0] && !message.attachments.size) return message.reply("```>meme [url] [tExTo] \n>meme [TeXtO] <imagen>```");
   if (message.attachments.size > 0) {
     if (!args[0]) return message.reply(":x: nohay ninguntexto aparteXD sigale,,,,");
-    let barra = [' | ', '|'];
+    let barra = ' | ';
     let toptext = args.join(' ').slice(0, barra);
     let bottomtext = args.slice(barra);
-    let mitad = Math.ceil(args.length / 2);
+    let mitad = Math.ceil(toptext.length / 2);
     if (bottomtext == undefined) bottomtext = args.slice(mitad);
     message.channel.startTyping();
     let imagen = message.attachments.first().url;
     let anchura = message.attachments.first().width;
     let altura = message.attachments.first().height;
     console.log(toptext);
-    console.log(bottomtext);
+    console.log(mitad);
     let opt = new meme({
       canvasOptions: {
         canvasWidth: anchura,
