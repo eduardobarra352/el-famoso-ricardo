97599ab8a8014afbacde25992713fee1c05d5b62
eduardobarra352 <42156451+eduardobarra352@users.noreply.github.com>
2019-07-24 01:09:20 -0400

    deviantnode o.o

--- bp/da.js
+++ bp/da.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const deviantnode = require('deviantnode');
 
 exports.run = async (bot, message, args, AttachImagen) => {
-  if (!args[0] || !['popular', 'undiscovered', 'newest', 'gallery'].includes(args[0])) return message.reply("lista de comandos relacionados a\nDeviantart\nDeviantart: ```\n\n>deviantart popular [opcional: algo q buscar] \n>deviantart undiscovered \n>deviantart newest \n>deviantart gallery [nombre de usuario]```");
+  if (!args[0] || !['popular', 'undiscovered', 'newest', 'gallery'].includes(args[0])) return message.reply("lista de comandos relacionados a\nDeviantart\nDeviantart: ```\n\n>deviantart popular [opcional: algo q buscar] \n>deviantart undiscovered \n>deviantart newest [opcional: algo q buscar] \n>deviantart gallery [nombre de usuario]```");
   if (args[0].includes("popular")) {
     let query = args[1];
     let responsable = message.author.id;
@@ -38,18 +38,18 @@ exports.run = async (bot, message, args, AttachImagen) => {
           .setAuthor(message.author.username, message.author.avatarURL)
           .setThumbnail('https://raw.githubusercontent.com/eduardobarra352/el-famoso-ricardo/master/img/deviantart.png')
           .setImage(imagenart)
-          .addField("Resultados:", (minim+1) + "-100")
+          .addField("Resultados:", (minim+1) + "-10")
           .setFooter(authorname+" | escribe un numero para ver los otros resultados o.o", authorpic);
           if (veces == 0) { message.channel.send(embed).then(msg => msgid = msg).then(setTimeout(()=>{ if (veces == 0) { embed.setFooter(authorname+' | se termino los resultados,,', authorpic); msgid.edit(embed); } },31000)); }
           try { request(res.results[i].content.src).pipe(fs.createWriteStream('da.png')); setTimeout(()=>{ AttachImagen('da.png', message.channel.id) },2000); } catch(e) { console.log(e); }
         }
         EmbedArt(minim);
-        filtro = m => !isNaN(m.content) && m.author.id == responsable && m.content < 100+1 && m.content > 0;
+        filtro = m => !isNaN(m.content) && m.author.id == responsable && m.content < 10+1 && m.content > 0;
         collector = message.channel.createMessageCollector(filtro, { time: 30000 });
         collector.res = res;
         collector.on('collect', m => {
           clearTimeout(timer);
-          if (m > 0 || m < 101) {
+          if (m > 0 || m < 11) {
               minim = m-1;
               veces = veces+1;
               resultados();
@@ -77,62 +77,55 @@ exports.run = async (bot, message, args, AttachImagen) => {
     let filtro;
     let collector;
     var timer;
-    request('http://barrarchiverio.7m.pl/a/access?client_id='+process.env.daclid+'&client_secret='+process.env.daclisecret, function (err, response, body) {
-      body = JSON.parse(body);
-      let url;
-      message.channel.startTyping();
-      function resultados() {
-        url = 'https://www.deviantart.com/api/v1/oauth2/browse/undiscovered?offset='+minim+'&limit='+nivel+'&access_token='+body.access_token;
-        request({ url: url, json: true }, function (err, response, body) { if (!err && response.statusCode === 200) {
-          if (err) return message.channel.send(":x: Uy, un erroralgo feo, mmmm siga intentando");
-          res = JSON.stringify(body, null, '  ');
-          res = JSON.parse(res);
-          let imagenart = '';
-          let tituloart = ''; 
-          let authorname = '';
-          let authorpic = '';
-          function EmbedArt (i) {
-              try { imagenart = res.results[i].content.src; } catch(e) { console.log(e); }
-              try { tituloart = res.results[i].title; } catch(e) { console.log(e); }
-              try { authorname = res.results[i].author.username; authorpic = res.results[i].author.usericon; } catch(e) { console.log(e); }
-              embed = new Discord.RichEmbed()
-              .setColor("#40f230")
-              .setTitle(tituloart)
-              .setURL(res.results[i].url)
-              .setAuthor(message.author.username, message.author.avatarURL)
-              .setThumbnail('https://raw.githubusercontent.com/eduardobarra352/el-famoso-ricardo/master/img/deviantart.png')
-              .setImage(imagenart)
-              .addField("Resultados:", (minim+1) + "-100")
-              .setFooter(authorname+" | escribe un numero para ver los otros resultados o.o", authorpic);
-              if (veces == 0) { message.channel.send(embed).then(msg => msgid = msg).then(setTimeout(()=>{ if (veces == 0) { embed.setFooter(authorname+' | se termino los resultados,,', authorpic); msgid.edit(embed); } },31000)); }
-              try { request(res.results[i].content.src).pipe(fs.createWriteStream('da.png')); setTimeout(()=>{ AttachImagen('da.png', message.channel.id) },2000); } catch(e) { console.log(e); }
-          }
-          EmbedArt(0);
-          filtro = m => !isNaN(m.content) && m.author.id == responsable && m.content < 100+1 && m.content > 0;
-          collector = message.channel.createMessageCollector(filtro, { time: 30000 });
-          collector.res = res;
-          collector.on('collect', m => {
-              clearTimeout(timer);
-              if (m > 0 || m < 101) {
-                minim = m-1;
-                veces = veces+1;
-                resultados();
-                setTimeout(()=>{ msgid.edit(embed); m.delete(); },1000);
-                timer = setTimeout(()=>{
-                  collector.on('end', v => {
-                setTimeout(()=>{ embed.setFooter('se termino los resultados,,'); msgid.edit(embed); },2000);
-                  });
-                },30000);
-              }
-          });
+    function resultados() {
+      deviantnode.getUndiscoveredDeviations(process.env.daclid, process.env.daclisecret)
+      .then(response => {
+        res = response;
+        let imagenart = '';
+        let tituloart = ''; 
+        let authorname = '';
+        let authorpic = '';
+        function EmbedArt (i) {
+          try { imagenart = res.results[i].content.src; } catch(e) { console.log(e); }
+          try { tituloart = res.results[i].title; } catch(e) { console.log(e); }
+          try { authorname = res.results[i].author.username; authorpic = res.results[i].author.usericon; } catch(e) { console.log(e); }
+          embed = new Discord.RichEmbed()
+          .setColor("#40f230")
+          .setTitle(tituloart)
+          .setURL(res.results[i].url)
+          .setAuthor(message.author.username, message.author.avatarURL)
+          .setThumbnail('https://raw.githubusercontent.com/eduardobarra352/el-famoso-ricardo/master/img/deviantart.png')
+          .setImage(imagenart)
+          .addField("Resultados:", (minim+1) + "-10")
+          .setFooter(authorname+" | escribe un numero para ver los otros resultados o.o", authorpic);
+          if (veces == 0) { message.channel.send(embed).then(msg => msgid = msg).then(setTimeout(()=>{ if (veces == 0) { embed.setFooter(authorname+' | se termino los resultados,,', authorpic); msgid.edit(embed); } },31000)); }
+          try { request(res.results[i].content.src).pipe(fs.createWriteStream('da.png')); setTimeout(()=>{ AttachImagen('da.png', message.channel.id) },2000); } catch(e) { console.log(e); }
         }
+        EmbedArt(minim);
+        filtro = m => !isNaN(m.content) && m.author.id == responsable && m.content < 10+1 && m.content > 0;
+        collector = message.channel.createMessageCollector(filtro, { time: 30000 });
+        collector.res = res;
+        collector.on('collect', m => {
+          clearTimeout(timer);
+          if (m > 0 || m < 11) {
+            minim = m-1;
+            veces = veces+1;
+            resultados();
+            setTimeout(()=>{ msgid.edit(embed); m.delete(); },1000);
+            timer = setTimeout(()=>{
+              collector.on('end', v => {
+                setTimeout(()=>{ embed.setFooter('se termino los resultados,,'); msgid.edit(embed); },2000);
+              });
+            },30000);
+          }
         });
-      }
-      resultados();
-      message.channel.stopTyping();
-    });
+      });
+    }
+    resultados();
+    message.channel.stopTyping();
   }
   if (args[0].includes("newest")) {
+    let query = args[1];
     let responsable = message.author.id;
     let res;
     let nivel = 1;
@@ -143,62 +136,54 @@ exports.run = async (bot, message, args, AttachImagen) => {
     let filtro;
     let collector;
     var timer;
-    request('http://barrarchiverio.7m.pl/a/access?client_id='+process.env.daclid+'&client_secret='+process.env.daclisecret, function (err, response, body) {
-      body = JSON.parse(body);
-      let url;
-      message.channel.startTyping();
-      function resultados() {
-        url = 'https://www.deviantart.com/api/v1/oauth2/browse/newest?offset='+minim+'&limit='+nivel+'&access_token='+body.access_token;
-        request({ url: url, json: true }, function (err, response, body) { if (!err && response.statusCode === 200) {
-          if (err) return message.channel.send(":x: Uy, un erroralgo feo, mmmm siga intentando");
-          res = JSON.stringify(body, null, '  ');
-          res = JSON.parse(res);
-          let imagenart = '';
-          let tituloart = ''; 
-          let authorname = '';
-          let authorpic = '';
-          let arturl;
-          function EmbedArt (i) {
-              try { imagenart = res.results[i].content.src; } catch(e) { console.log(e); }
-              try { tituloart = res.results[i].title; } catch(e) { console.log(e); }
-              try { arturl = res.results[i].url; } catch(e) { console.log(e); }
-              try { authorname = res.results[i].author.username; authorpic = res.results[i].author.usericon; } catch(e) { console.log(e); }
-              embed = new Discord.RichEmbed()
-              .setColor("#40f230")
-              .setTitle(tituloart)
-              .setURL(arturl)
-              .setAuthor(message.author.username, message.author.avatarURL)
-              .setThumbnail('https://raw.githubusercontent.com/eduardobarra352/el-famoso-ricardo/master/img/deviantart.png')
-              .setImage(imagenart)
-              .addField("Resultados:", (minim+1) + "-100")
-              .setFooter(authorname+" | escribe un numero para ver los otros resultados o.o", authorpic);
-              if (veces == 0) { message.channel.send(embed).then(msg => msgid = msg).then(setTimeout(()=>{ if (veces == 0) { embed.setFooter(authorname+' | se termino los resultados,,', authorpic); msgid.edit(embed); } },31000)); }
-              try { request(res.results[i].content.src).pipe(fs.createWriteStream('da.png')); setTimeout(()=>{ AttachImagen('da.png', message.channel.id) },2000); } catch(e) { console.log(e); }
-          }
-          EmbedArt(0);
-          filtro = m => !isNaN(m.content) && m.author.id == responsable && m.content < 100+1 && m.content > 0;
-          collector = message.channel.createMessageCollector(filtro, { time: 30000 });
-          collector.res = res;
-          collector.on('collect', m => {
-              clearTimeout(timer);
-              if (m > 0 || m < 101) {
-                minim = m-1;
-                veces = veces+1;
-                resultados();
-                setTimeout(()=>{ msgid.edit(embed); m.delete(); },1000);
-                timer = setTimeout(()=>{
-                  collector.on('end', v => {
-                setTimeout(()=>{ embed.setFooter('se termino los resultados,,'); msgid.edit(embed); },2000);
-                  });
-                },30000);
-              }
-          });
+    function resultados() {
+      deviantnode.getNewestDeviations(process.env.daclid, process.env.daclisecret, query)
+      .then(response => {
+        res = response;
+        let imagenart = '';
+        let tituloart = ''; 
+        let authorname = '';
+        let authorpic = '';
+        let arturl;
+        function EmbedArt (i) {
+          try { imagenart = res.results[i].content.src; } catch(e) { console.log(e); }
+          try { arturl = res.results[i].url; } catch(e) { console.log(e); }
+          try { tituloart = res.results[i].title; } catch(e) { console.log(e); }
+          try { authorname = res.results[i].author.username; authorpic = res.results[i].author.usericon; } catch(e) { console.log(e); }
+          embed = new Discord.RichEmbed()
+          .setColor("#40f230")
+          .setTitle(tituloart)
+          .setURL(arturl)
+          .setAuthor(message.author.username, message.author.avatarURL)
+          .setThumbnail('https://raw.githubusercontent.com/eduardobarra352/el-famoso-ricardo/master/img/deviantart.png')
+          .setImage(imagenart)
+          .addField("Resultados:", (minim+1) + "-10")
+          .setFooter(authorname+" | escribe un numero para ver los otros resultados o.o", authorpic);
+          if (veces == 0) { message.channel.send(embed).then(msg => msgid = msg).then(setTimeout(()=>{ if (veces == 0) { embed.setFooter(authorname+' | se termino los resultados,,', authorpic); msgid.edit(embed); } },31000)); }
+          try { request(res.results[i].content.src).pipe(fs.createWriteStream('da.png')); setTimeout(()=>{ AttachImagen('da.png', message.channel.id) },2000); } catch(e) { console.log(e); }
         }
+        EmbedArt(minim);
+        filtro = m => !isNaN(m.content) && m.author.id == responsable && m.content < 10+1 && m.content > 0;
+        collector = message.channel.createMessageCollector(filtro, { time: 30000 });
+        collector.res = res;
+        collector.on('collect', m => {
+          clearTimeout(timer);
+          if (m > 0 || m < 11) {
+              minim = m-1;
+              veces = veces+1;
+              resultados();
+              setTimeout(()=>{ msgid.edit(embed); m.delete(); },1000);
+              timer = setTimeout(()=>{
+                collector.on('end', v => {
+                  setTimeout(()=>{ embed.setFooter('se termino los resultados,,'); msgid.edit(embed); },2000);
+                });
+              },30000);
+          }
         });
-      }
-      resultados();
-      message.channel.stopTyping();
-    });
+      });
+    }
+    resultados();
+    message.channel.stopTyping();
   }
   if (args[0].includes("gallery")) {
     if (!args[1]) return message.reply("```>deviantart gallery [nombre de usuario]```");
